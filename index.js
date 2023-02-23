00ac613bbd06dfebce0e3e6e1d54a31bf9229fe2
teak1yari <danya.server@gmail.com>
2023-02-24 00:09:50 +0800

    добавил jwt

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
 app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Вася",
+    },
+    "secret123"
+  );
+
   res.json({
     success: true,
+    token,
   });
 });
 
