8df134155c5d5631e3a302ec5bcbddbaba58c4fd
teak1yari <danya.server@gmail.com>
2023-02-23 23:25:03 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/auth/login", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
