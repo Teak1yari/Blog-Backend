eba4e6924124b6d0da8078be4356b1995b8207bb
teak1yari <danya.server@gmail.com>
2023-03-05 21:29:54 +0800

    создали пользователя в бд, зашифровали пароль с помощью bcrypt, скрыли пароль юзера из ответа

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidator } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234@cluster0.pbo1xg1.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB Error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.post("/auth/register", registerValidator, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const token = jwt.sign(
-    {
+    const doc = UserModel({
       email: req.body.email,
-      fullName: "Вася",
-    },
-    "secret123"
-  );
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-  res.json({
-    success: true,
-    token,
-  });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегистрироваться",
+    });
+  }
 });
 
 app.listen(4444, (err) => {
