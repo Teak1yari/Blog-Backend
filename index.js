2da216ec1713fd8ef510309e5e85c23ac3a18fa4
teak1yari <danya.server@gmail.com>
2023-03-16 16:47:24 +0800

    сделал создание статей, валидация

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
-import { registerValidator } from "./validations/auth.js";
+import {
+  registerValidator,
+  loginValidator,
+  postCreateValidation,
+} from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -17,9 +22,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/login", UserController.login);
+app.post("/auth/login", loginValidator, UserController.login);
 app.post("/auth/register", registerValidator, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+//app.get("/posts", PostController.getAll);
+//app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+//app.delete("/posts", PostController.remove);
+//app.patch("/posts", PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
---
+++ validations.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+export const loginValidator = [
+  body("email", "Неверный формат почты!").isEmail(),
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+];
+
+export const registerValidator = [
+  body("email", "Неверный формат почты!").isEmail(),
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+  body("fullName", "Укажите имя").isLength({ min: 3 }),
+  body("avatarUrl", "Неверная ссылка на автарку").optional().isURL(),
+];
+
+export const postCreateValidation = [
+  body("title", "Введите заголовок статьи").isLength({ min: 3 }).isString(),
+  body("text", "Введите текст статьи").isLength({ min: 10 }),
+  body("tags", "Неверный формат тэгов (укажите массив)").optional().isString(),
+  body("imageUrl", "Неверная ссылка на изображение").optional().isString(),
+];
